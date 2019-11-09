5bfe90ba371f647c3a7b62632447947f5602ac31
Tejas Khare <tjskhare@gmail.com>
2019-11-09 17:00:19 -0500

    Add files via upload

---
+++ ClearNutrition/myapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url, include
+
+urlpatterns = [
+    path('',views.home,name = 'myapp-home'),
+    path('upload/',views.upload,name = 'myapp-upload'),
+    path('information/',views.information,name = 'myapp-information'),
+    path('diseases/',views.information1,name = 'myapp-diseases'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
